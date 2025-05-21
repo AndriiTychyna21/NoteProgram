567877034265a0cccdd65fc315c08205c66b4acd
AndriiTychyna21 <andriitychyna2104@gmail.com>
2025-05-21 23:38:21 +0300

    Notifications and Logger

--- Tag.ts
+++ Tag.ts
@@ -1,6 +1,12 @@
 export class Tag{
   constructor(
     private name: string,
-    private color: string
+    private color: string,
+    public font: string,
+    public iconPath: string
   ){}
+
+  public clone(): Tag {
+    return new Tag(this.name, this.color, this.font, this.iconPath);
+  }
 }
